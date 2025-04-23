de20eef35ddc2be85fdd6db2cc58ceabe20f73e1
Yuhao Lin <139048798+yuha0lin@users.noreply.github.com>
2025-04-24 00:03:26 +0800

    v0.3 (#33)
    * Organize the file structure
    
    * upload MappingTable
    
    * Update color scheme
    
    * upload favicon and images
    
    * Update processor to accommodate data template changes
    
    * update signature
    
    * update gitignore
    
    * init FHL-JSONLD
    
    * update test examples
    
    * rebuild test example

--- backEnd/backend.py
+++ backEnd/backend.py
@@ -101,9 +101,5 @@ def handle_query():
     return jsonify(response)
 
 
-
-
-# 
-
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
